a0f940318a3c573c355739a912a0b335652f7260
acallah1 <acallah1@nd.edu>
2018-12-12 22:42:04 -0500

    working?

--- predict_upvotes.py
+++ predict_upvotes.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import os
 import sys
 import csv
 import argparse
@@ -7,13 +8,20 @@ from sklearn import linear_model
 
 
 def transform_scored_comments(data_file_path):
+
+	new_data_file_path=data_file_path[0:-4]+"_mod.csv"
+
+	if os.path.isfile(new_data_file_path):
+		print("Train file already exists")
+		return new_data_file_path
+
 	word_list = []
 	data_object_list = []
 
 	print("read in start")
 
 	with open(data_file_path, "r", encoding="utf-8") as csv_file:
-		csv_reader = csv.reader(csv_file, delimiter=",")
+		csv_reader = csv.reader(csv_file, delimiter="\t")
 		for row in csv_reader:
 			try:
 				data_object_list.append(row)
@@ -38,18 +46,19 @@ def transform_scored_comments(data_file_path):
 		new_data_object[-1] = data_object[-1]
 		new_data_object_list.append(new_data_object)
 
-	new_data_file_path=data_file_path[0:-4]+"_mod.csv"
+	
 	with open(new_data_file_path, "w+", encoding="utf-8") as output_file:
 		for word in word_list:
-			output_file.write("{},".format(word))
+			output_file.write("{}\t".format(word))
 		output_file.write("score")
 		output_file.write("\n")
 		for data_object in new_data_object_list:
 			for feature in data_object:
-				output_file.write("{},".format(feature))
+				output_file.write("{}\t".format(feature))
 			output_file.write("\n")
 
-
+	print("Done")
+	return new_data_file_path
 
 def parse_command_line():
 	parser = argparse.ArgumentParser()
@@ -74,6 +83,11 @@ def parse_command_line():
 						type=int,
 						dest="state_size",
 						default=2)
+	parser.add_argument("-t", "--train_file",
+						type=str,
+						dest="train_file",
+						required=True)
+
 
 	return parser.parse_args()
 
@@ -86,8 +100,25 @@ if __name__ == ("__main__"):
 	input_files = arguments.input_files
 	weights = arguments.weights
 	state_size = arguments.state_size
+	train_file = arguments.train_file
 
 	number_comments = arguments.number_comments
+
+	new_train_file = transform_scored_comments(train_file)
+
+	features = list()
+	scores = list()
+	labels = None
+	with open(new_train_file, "r", encoding="utf-8") as csv_file:
+		i = 0
+		csv_reader = csv.reader(csv_file, delimiter="\t")
+		labels = next(csv_reader)
+		for row in csv_reader:
+			features.append(list())
+			for feature in row[0:-1]:
+				features[i].append(feature)
+			scores.append(0 if row[-1] == '' else int(row[-1]))
+			i = i + 1
 
 	if len(input_files) is not len(weights):
 			raise ValueError("must specify the same number of input files and weights")
@@ -96,15 +127,27 @@ if __name__ == ("__main__"):
 	for i in range(0,len(input_files)):
 		input_file_weight_pairs.append((input_files[i],weights[i]))
 
+	print("Starting fit")
+	linear_regression_model = linear_model.LinearRegression()
+	linear_regression_model.fit(features,scores, verbosity = 10)
+
+	print("Done fitting")
 	for i in range(0, number_comments):
 
-		generated_comment = erinhhbot.generate_comment(input_file_weight_pairs, state_size)
+		generated_comment = erinhhbot.generate_comment(input_file_weight_pairs, state_size,verbosity = 2)
 		predicted_upvotes = 0
 
+		comment_features = list()
+		for l in labels:
+			if l in generated_comment.split():
+				comment_features.append('1,')
+			else:
+				comment_features.append('0,')
+		print("Predicting")
+		print(linear_regression_model.predict(comment_features))
 		# To do: predict upvotes
 		# going to have to transform the data in /scored_comments to work, probably want to use pandas dataframes
 
-		linear_regression_model = linear_model.LinearRegression()
-		linear_regression_model.fit(X,y)
+		
 
-		print("generated_comment: {}, expected score: {}".format(generated_comment, predicted_upvotes))
+		#print("generated_comment: {}, expected score: {}".format(generated_comment, predicted_upvotes))
