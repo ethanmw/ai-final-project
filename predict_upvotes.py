72ba1fa407e89aae46f00bff2b8f81ad78017e2c
acallah1 <acallah1@nd.edu>
2018-12-12 19:51:38 -0500

    adding correct arguments to predict comment

--- predict_upvotes.py
+++ predict_upvotes.py
@@ -11,17 +11,48 @@ def parse_command_line():
 						type=int,
 						dest="number_comments",
 						default=1)
+	parser.add_argument("-i", "--input-file",
+						action="append",
+						dest="input_files",
+						required=True)
+
+	parser.add_argument("-w", "--weight",
+						type=float,
+						action="append",
+						dest="weights",
+						required=True)
+	parser.add_argument("-s", "--state-size",
+						type=int,
+						dest="state_size",
+						default=2)
+
 
 	return parser.parse_args()
 
 
 if __name__ == ("__main__"):
+
+
 	arguments = parse_command_line()
+
+	input_files = arguments.input_files
+	weights = arguments.weights
+	state_size = arguments.state_size
+
+
 
 	number_comments = arguments.number_comments
 
+	if len(input_files) is not len(weights):
+			raise ValueError("must specify the same number of input files and weights")
+
+	input_file_weight_pairs = []
+	for i in range(0,len(input_files)):
+		input_file_weight_pairs.append((input_files[i],weights[i]))
+
 	for i in range(0, number_comments):
-		generated_comment = erinhhbot.generate_comment()
+
+		generated_comment = erinhhbot.generate_comment(input_file_weight_pairs, state_size)
 		predicted_upvotes = 0
 
 		# To do: predict upvotes
